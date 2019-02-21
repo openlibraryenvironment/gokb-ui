4f4739f04f2f688ec94cdc0db580de3e805cb316
d.ecker <d.ecker@fz-juelich.de>
2019-02-21 11:25:10 +0100

    search uses find service

--- src/shared/services/search-services/search-services.js
+++ src/shared/services/search-services/search-services.js
@@ -1,4 +1,4 @@
-const SEARCH_URL = './find'
+const SEARCH_URL = './suggest'
 
 const HEADERS = {
   'Authorization': `Basic ${btoa(`${process.env.VUE_APP_API_USER}:${process.env.VUE_APP_API_PASSWORD}`)}`
@@ -6,7 +6,10 @@ const HEADERS = {
 
 const api = http => ({
   async globalSearch (searchPattern, cancelToken) {
-    const searchResult = await http.request({ method: 'GET', HEADERS, url: `${SEARCH_URL}?name=${searchPattern}`, cancelToken })
+    if (!searchPattern) {
+      return { records: [] }
+    }
+    const searchResult = await http.request({ method: 'GET', HEADERS, url: `${SEARCH_URL}?q=${searchPattern}`, cancelToken })
     return searchResult.data
   },
 })
