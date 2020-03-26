5cfa0851475613e09ad7b43873810486c9584e66
Ecker, Dirk <d.ecker@fz-juelich.de>
2020-03-26 11:57:24 +0100

    added package services

---
+++ src/shared/services/package-services/index.js
@@ -0,0 +1,5 @@
+import baseServices from '@/shared/services/base-services'
+
+import api from './package-services'
+
+export default api(baseServices)
---
+++ src/shared/services/package-services/package-services.js
@@ -0,0 +1,11 @@
+const api = (baseServices) => ({
+  // search (parameter, cancelToken) {
+  //   const urlParameter = baseServices.createQueryParameters(parameter)
+  //   const url = `${REST_ENDPOINT}?${urlParameter}`
+  //   return baseServices.request({
+  //     method: 'GET', url,
+  //   }, cancelToken)
+  // },
+})
+
+export default api
