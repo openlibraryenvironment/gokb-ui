be957e520585ddf94520394366ad2b1bf2fc7181
Ecker, Dirk <d.ecker@fz-juelich.de>
2020-04-27 15:00:24 +0200

    set the correct base url to access the rest backend

--- src/shared/services/http.js
+++ src/shared/services/http.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const http = axios.create()
+const http = axios.create({
+  baseURL: process.env.VUE_APP_API_BASE_URL,
+})
 
 const createCancelToken = () => axios.CancelToken.source()
 const isCancelThrown = (error) => axios.isCancel(error)
