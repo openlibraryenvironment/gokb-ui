ab64bd2ca2875cf3f83c154b40548f9c6a931ca8
Ecker, Dirk <d.ecker@fz-juelich.de>
2020-04-27 16:59:21 +0200

    support local development without cors problems

--- src/shared/services/http.js
+++ src/shared/services/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+console.log(process.env.VUE_APP_API_BASE_LOCAL)
 const http = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL,
+  ...(process.env.VUE_APP_API_BASE_LOCAL ? {} : { baseURL: process.env.VUE_APP_API_BASE_URL }),
 })
 
 const createCancelToken = () => axios.CancelToken.source()
