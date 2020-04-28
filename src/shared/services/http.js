0749dbf800a6c3732a726afac48d1a9d647f67c6
Ecker, Dirk <d.ecker@fz-juelich.de>
2020-04-28 14:55:11 +0200

    comment removed

--- src/shared/services/http.js
+++ src/shared/services/http.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-console.log(process.env.VUE_APP_API_BASE_LOCAL)
+
 const http = axios.create({
   ...(process.env.VUE_APP_API_BASE_LOCAL ? {} : { baseURL: process.env.VUE_APP_API_BASE_URL }),
 })
