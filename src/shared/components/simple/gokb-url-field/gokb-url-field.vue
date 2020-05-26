2e3b9bb6adca1569174215f6716ff7ac624c9c6e
Ecker, Dirk <d.ecker@fz-juelich.de>
2020-05-26 08:06:54 +0200

    added url field

---
+++ src/shared/components/simple/gokb-url-field/gokb-url-field.vue
@@ -0,0 +1,15 @@
+<script>
+  import GokbTextField from '@/shared/components/base/gokb-text-field'
+
+  export default {
+    name: 'GokbUrlField',
+    extends: GokbTextField,
+    props: {
+      type: {
+        type: String,
+        required: false,
+        default: 'url'
+      }
+    },
+  }
+</script>
