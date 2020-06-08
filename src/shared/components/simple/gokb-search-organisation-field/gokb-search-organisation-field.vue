24c346939e232ad3f2c4f1c4675a4644f1d4caf4
Ecker, Dirk <d.ecker@fz-juelich.de>
2020-06-08 13:28:02 +0200

    added new component to search for organisations

---
+++ src/shared/components/simple/gokb-search-organisation-field/gokb-search-organisation-field.vue
@@ -0,0 +1,18 @@
+<script>
+  import GokbSearchField from '@/shared/components/base/gokb-search-field'
+
+  export default {
+    name: 'GokbSearchProviderField',
+    extends: GokbSearchField,
+    props: {
+      label: {
+        type: String,
+        required: false,
+        default: 'Provider'
+      }
+    },
+    created () {
+      this.searchServicesResourceUrl = 'rest/orgs'
+    },
+  }
+</script>
---
+++ src/shared/components/simple/gokb-search-organisation-field/index.js
@@ -0,0 +1,1 @@
+export default from './gokb-search-organisation-field'
