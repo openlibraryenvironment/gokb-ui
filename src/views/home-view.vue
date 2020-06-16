2180ad1796e879a1f38ed19076fc969a501da55e
Ecker, Dirk <d.ecker@fz-juelich.de>
2020-06-16 11:18:10 +0200

    get rid of errors on page home

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -14,9 +14,11 @@
         />
       </template>
       <gokb-table
-        :show-select="false"
-        :headers="reviewsHeader"
         :items="reviews"
+        :headers="reviewsHeader"
+        :selected-items="selectedReviews"
+        :total-number-of-items="totalNumberOfReviews"
+        :options.sync="reviewsOptions"
       />
     </gokb-section>
     <gokb-section sub-title="Pflege">
@@ -30,16 +32,20 @@
         />
       </template>
       <gokb-table
-        :show-select="false"
-        :headers="maintenancesHeader"
         :items="maintenances"
+        :headers="maintenancesHeader"
+        :selected-items="selectedMaintenances"
+        :total-number-of-items="totalNumberOfMaintenances"
+        :options.sync="maintenancesOptions"
       />
     </gokb-section>
     <gokb-section sub-title="KBART Import">
       <gokb-table
-        :show-select="false"
-        :headers="kbartImportsHeader"
         :items="kbartImports"
+        :headers="kbartImportsHeader"
+        :selected-items="selectedKbartImports"
+        :total-number-of-items="totalNumberOfKbartImports"
+        :options.sync="kbartImportsOptions"
       />
     </gokb-section>
   </gokb-page>
@@ -48,89 +54,125 @@
 <script>
   import LoginPopup from '@/shared/popups/gokb-login-popup'
 
+  const ROWS_PER_PAGE = 10
+
+  const REVIEWS_HEADER = [
+    {
+      text: 'Name',
+      align: 'left',
+      sortable: false,
+      value: 'name'
+    },
+    {
+      text: 'Typ',
+      align: 'left',
+      sortable: false,
+      value: 'type'
+    },
+    {
+      text: 'Erstellt am',
+      align: 'left',
+      sortable: false,
+      value: 'createdDate'
+    },
+    {
+      text: 'Ersteller',
+      align: 'left',
+      sortable: false,
+      value: 'creator'
+    },
+  ]
+  const MAINTENANCES_HEADER = [
+    {
+      text: 'Name',
+      align: 'left',
+      sortable: false,
+      value: 'name'
+    },
+    {
+      text: 'Typ',
+      align: 'left',
+      sortable: false,
+      value: 'type'
+    },
+    {
+      text: 'Fällig am',
+      align: 'left',
+      sortable: false,
+      value: 'dueDate'
+    },
+  ]
+  const KBARTIMPORTS_HEADER = [
+    {
+      text: 'Dateiname',
+      align: 'left',
+      sortable: false,
+      value: 'filename'
+    },
+    {
+      text: 'Importiert am',
+      align: 'left',
+      sortable: false,
+      value: 'importDate'
+    },
+  ]
+
   export default {
     name: 'ProfileView',
     components: { LoginPopup },
     data () {
       return {
         showLogin: false,
-        reviewsHeader: [
-          {
-            text: 'Name',
-            align: 'left',
-            sortable: false,
-            value: 'name'
-          },
-          {
-            text: 'Typ',
-            align: 'left',
-            sortable: false,
-            value: 'type'
-          },
-          {
-            text: 'Erstellt am',
-            align: 'left',
-            sortable: false,
-            value: 'createdDate'
-          },
-          {
-            text: 'Ersteller',
-            align: 'left',
-            sortable: false,
-            value: 'creator'
-          },
-        ],
-        maintenancesHeader: [
-          {
-            text: 'Name',
-            align: 'left',
-            sortable: false,
-            value: 'name'
-          },
-          {
-            text: 'Typ',
-            align: 'left',
-            sortable: false,
-            value: 'type'
-          },
-          {
-            text: 'Fällig am',
-            align: 'left',
-            sortable: false,
-            value: 'dueDate'
-          },
-        ],
-        kbartImportsHeader: [
-          {
-            text: 'Dateiname',
-            align: 'left',
-            sortable: false,
-            value: 'filename'
-          },
-          {
-            text: 'Importiert am',
-            align: 'left',
-            sortable: false,
-            value: 'importDate'
-          },
-        ],
-        reviews: [
-          { name: 'American Science Journal', type: 'Einzeltitel', createdDate: '22.01.2018 12:45', creator: 'Jochen Schweitzer' },
-          { name: 'Springer Best Journals', type: 'Paket', createdDate: '11.04.2018 16:23', creator: 'Frank Thelen' },
-        ],
-        maintenances: [
-          { name: 'American Science Journal', type: 'Einzeltitel', dueDate: '01.12.2018' },
-          { name: 'Springer Best Journals', type: 'Paket', dueDate: '06.01.2019' },
-        ],
-        kbartImports: [
-          { filename: 'American Science Journal.tsv', importDate: '01.12.2018 12:43' },
-        ],
+        reviews: [],
+        selectedReviews: [],
+        totalNumberOfReviews: undefined,
+        reviewsOptions: {
+          page: 1,
+          itemsPerPage: ROWS_PER_PAGE
+        },
+
+        maintenances: [],
+        selectedMaintenances: [],
+        totalNumberOfMaintenances: undefined,
+        maintenancesOptions: {
+          page: 1,
+          itemsPerPage: ROWS_PER_PAGE
+        },
+
+        kbartImports: [],
+        selectedKbartImports: [],
+        totalNumberOfKbartImports: undefined,
+        kbartImportsOptions: {
+          page: 1,
+          itemsPerPage: ROWS_PER_PAGE
+        },
       }
     },
     activated () {
       if (this.$route.query.login) {
         this.showLogin = true
       }
+    },
+    created () {
+      this.reviewsHeader = REVIEWS_HEADER
+      this.maintenancesHeader = MAINTENANCES_HEADER
+      this.kbartImportsHeader = KBARTIMPORTS_HEADER
+
+      // todo: replace dummy data with backend requests
+      this.reviews = [
+        { name: 'American Science Journal', type: 'Einzeltitel', createdDate: '22.01.2018 12:45', creator: 'Jochen Schweitzer' },
+        { name: 'Springer Best Journals', type: 'Paket', createdDate: '11.04.2018 16:23', creator: 'Frank Thelen' },
+      ]
+      this.maintenances = [
+        { name: 'American Science Journal', type: 'Einzeltitel', dueDate: '01.12.2018' },
+        { name: 'Springer Best Journals', type: 'Paket', dueDate: '06.01.2019' },
+      ]
+      this.kbartImports = [
+        { filename: 'American Science Journal.tsv', importDate: '01.12.2018 12:43' },
+      ]
+      this.totalNumberOfReviews = 2
+      this.totalNumberOfMaintenances = 2
+      this.totalNumberOfKbartImports = 2
     }
   }
 </script>
