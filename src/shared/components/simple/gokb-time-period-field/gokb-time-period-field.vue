78d1fde52e3fa43d974a45a9f4a53c10ad0de2a1
Ecker, Dirk <d.ecker@fz-juelich.de>
2020-07-06 10:51:16 +0200

    added time period component

---
+++ src/shared/components/simple/gokb-time-period-field/gokb-time-period-field.vue
@@ -0,0 +1,36 @@
+<script>
+  import GokbSelectField from '@/shared/components/base/gokb-select-field'
+
+  export default {
+    name: 'GokbTimePeriodField',
+    extends: GokbSelectField,
+    props: {
+      label: {
+        type: String,
+        required: false,
+        default: 'Einheit',
+      },
+      returnObject: {
+        type: Boolean,
+        required: false,
+        default: false
+      },
+    },
+    created () {
+      this.items = [
+        {
+          id: 'D',
+          name: 'Tag(e)'
+        },
+        {
+          id: 'M',
+          name: 'Monat(e)'
+        },
+        {
+          id: 'Y',
+          name: 'Jahr(e)',
+        }
+      ]
+    }
+  }
+</script>
---
+++ src/shared/components/simple/gokb-time-period-field/index.js
@@ -0,0 +1,1 @@
+export default from './gokb-time-period-field'
