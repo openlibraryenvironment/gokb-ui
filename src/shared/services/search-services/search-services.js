51d1fef93964ae6e35ab3233963ccbcbdbc18f19
Ecker, Dirk <d.ecker@fz-juelich.de>
2020-07-07 16:47:07 +0200

    adjust urls for local development

--- src/shared/services/search-services/search-services.js
+++ src/shared/services/search-services/search-services.js
@@ -1,25 +1,5 @@
 const api = baseServices =>
     resourceUrl => ({
-  // async globalSearch ({ searchPattern, componentType = '', max = 10 }, cancelToken) {
-  //   if (!searchPattern) {
-  //     return { records: [] }
-  //   }
-  //   const searchResult = await baseServices.request({
-  //     method: 'GET',
-  //     url: `${GLOBAL_SEARCH_URL}?q=${searchPattern}&componentType=${componentType}&max=${max}`
-  //   }, cancelToken)
-  //   return searchResult.data
-  // },
-  //
-  // async find (parameters = { max: 10, offset: 0 }, cancelToken) {
-  //   const queryParameters = baseServices.createQueryParameters(parameters)
-  //   const searchResult = await baseServices.request({
-  //     method: 'GET',
-  //     url: `${FIND_URL}?${queryParameters}`
-  //   }, cancelToken)
-  //   return searchResult.data
-  // },
-
   search (parameter, cancelToken) {
     const urlParameter = baseServices.createQueryParameters(parameter)
     const url = `${resourceUrl}?${urlParameter}`
@@ -31,14 +11,14 @@ const api = baseServices =>
   delete (url, cancelToken) {
     return baseServices.request({
       method: 'DELETE',
-      url,
+      url: baseServices.relativeUrl(url),
     }, cancelToken)
   },
 
   retire (url, cancelToken) {
     return baseServices.request({
       method: 'POST',
-      url,
+      url: baseServices.relativeUrl(url),
     }, cancelToken)
   },
 })
