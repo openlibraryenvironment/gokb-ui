8ad99a9a919bdc190801bc93e6d2880fed5b66d4
Ecker, Dirk <d.ecker@fz-juelich.de>
2020-07-13 15:02:31 +0200

    packages updated, kbart import implementation

--- src/shared/components/base/gokb-file-input-field/gokb-file-input-field.vue
+++ src/shared/components/base/gokb-file-input-field/gokb-file-input-field.vue
@@ -1,12 +1,13 @@
 <template>
   <v-file-input
-    v-bind="$attrs"
-    prepend-icon=""
+    v-model="localSelectedFile"
+    :label="label"
+    prepend-icon="mdi-file-delimited"
+    :disabled="disabled"
+    counter
+    accept=".txt"
     show-size
-    clearable
-  >
-  &nbsp;
-  </v-file-input>
+  />
 </template>
 
 <script>
@@ -16,9 +17,29 @@
       label: {
         type: String,
         required: false,
-        default: undefined,
+        default: 'Datei',
+      },
+      disabled: {
+        type: Boolean,
+        required: false,
+        default: false,
+      },
+      selectedFile: {
+        type: String,
+        required: false,
+        default: '',
       },
     },
+    computed: {
+      localSelectedFile: {
+        get () {
+          return this.selectedFile
+        },
+        set (value) {
+          this.$emit('input', value)
+        }
+      }
+    }
   }
 </script>
 
--- src/shared/popups/gokb-kbart-import-popup/gokb-kbart-import-popup.vue
+++ src/shared/popups/gokb-kbart-import-popup/gokb-kbart-import-popup.vue
@@ -5,12 +5,24 @@
     @submit="importKbart"
   >
     <gokb-section sub-title="Optionen">
-      <gokb-file-input-field label="Datei" />
-      <gokb-select-field label="Titel-ID Namensraum" />
-      <gokb-checkbox-field label="Weitere Identifier sind zu beachten" />
-      <v-progress-linear :height="20">
-        <template v-slot="{ value1 }">
-          <strong>{{ value1 }}%</strong>
+      <gokb-file-input-field
+        v-model="selectedFile"
+        :disabled="importRunning"
+      />
+      <gokb-namespace-field
+        v-model="selectedNamespace"
+        label="Titel-ID Namensraum"
+      />
+      <gokb-checkbox-field
+        v-model="considerAdditionalIdentifier"
+        label="Weitere Identifier sind zu beachten"
+      />
+      <v-progress-linear
+        height="25"
+        :value="completion"
+      >
+        <template #default="{ value: v }">
+          <strong>{{ v }}%</strong>
         </template>
       </v-progress-linear>
     </gokb-section>
@@ -22,7 +34,11 @@
       >
         Abbrechen
       </gokb-button>
-      <gokb-button default>
+      <gokb-button
+        default
+        :disabled="!selectedFile || importRunning"
+        @click="doImport"
+      >
         Import
       </gokb-button>
     </template>
@@ -31,9 +47,12 @@
 
 <script>
   import BaseComponent from '@/shared/components/base-component'
+  import math from '@/shared/utils/math'
+  import GokbNamespaceField from '@/shared/components/simple/gokb-namespace-field'
 
   export default {
     name: 'GokbKbartImportPopup',
+    components: { GokbNamespaceField },
     extends: BaseComponent,
     props: {
       value: {
@@ -45,6 +64,11 @@
     data () {
       return {
         error: undefined,
+        selectedNamespace: undefined,
+        considerAdditionalIdentifier: undefined,
+        importRunning: undefined,
+        selectedFile: undefined,
+        completion: undefined,
       }
     },
     computed: {
@@ -64,7 +88,37 @@
       importKbart () {
         console.log('do import')
         this.close()
-      }
+      },
+      doImport () {
+        this.error = undefined
+        this.importRunning = true
+        this.completion = 0
+        this.readerForImport = new FileReader()
+        this.readerForImport.onload = this._importCompleted
+        this.readerForImport.onprogress = this._importProgress
+        this.readerForImport.readAsText(this.selectedFile)
+      },
+      async _importCompleted () {
+        const csvDataRows = this.readerForImport.result.split(/\r?\n/)
+          .filter(row => row.trim())
+        if (csvDataRows.length === 0) {
+          return
+        }
+        try {
+          // await baseServices.pubchargedb({
+          //   initiator: this._importCompleted.name,
+          //   data: rowsAsJson
+          // })
+        } catch (exception) {
+          this.error = exception
+        } finally {
+          this.completion = 100
+          this.importRunning = false
+        }
+      },
+      _importProgress ({ loaded, total }) {
+        this.completion = math.asPercent(loaded / total)
+      },
     }
   }
 </script>
