535d4e39dd2e6a300d4fcf6fdc4f6842854bda6c
Ecker, Dirk <d.ecker@fz-juelich.de>
2020-07-29 08:12:00 +0200

    added coverage input field

---
+++ src/shared/components/simple/gokb-coverage-field/gokb-coverage-field.vue
@@ -0,0 +1,24 @@
+<script>
+  import GokbSelectField from '@/shared/components/base/gokb-select-field'
+
+  export default {
+    name: 'GokbCoverageField',
+    extends: GokbSelectField,
+    props: {
+      label: {
+        type: String,
+        required: false,
+        default: 'Art',
+      }
+    },
+    created () {
+      this.entityName = 'coverage-depth'
+    },
+    methods: {
+      transform (result) {
+        const { data: { data: values } } = result || { data: { data: [] } }
+        return values.map(({ id, value }) => ({ id, name: value }))
+      }
+    }
+  }
+</script>
