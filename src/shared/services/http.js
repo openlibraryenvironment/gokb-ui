a48316604c68ba66ec555dfc41189acd67dd0d04
d.ecker <d.ecker@fz-juelich.de>
2020-08-28 14:59:33 +0200

    back to correct implementation of url

--- src/shared/services/http.js
+++ src/shared/services/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL
+  ...(process.env.VUE_APP_API_BASE_LOCAL ? {} : { baseURL: process.env.VUE_APP_API_BASE_URL }),
 })
 
 const createCancelToken = () => axios.CancelToken.source()
