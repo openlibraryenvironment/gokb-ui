fa2e968fb5991feefc7cf2d4761899a05a891889
Philipp von Böselager <philipp.boeselager@web.de>
2020-10-21 16:44:04 +0200

    Small fixes

--- src/shared/services/kbart-services/kbart-services.js
+++ src/shared/services/kbart-services/kbart-services.js
@@ -3,7 +3,7 @@ const api = (baseServices) => ({
     upload (file, parameters, cancelToken) {
       const urlParameters = baseServices.createQueryParameters(parameters)
       const data = new FormData().append('file', file)
-      const url = process.env.VUE_APP_YGOR_BASE_URL + `/enrichment/processCompleteNoInteraction?${urlParameters}`
+      const url = process.env.VUE_APP_YGOR_BASE_URL + `/enrichment/processCompleteWithToken?${urlParameters}`
       const result = baseServices.request({
         method: 'POST',
         url,
