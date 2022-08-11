a78e9fcfce81e465f637e9a9667f6c60fecf20e2
Philipp v. Böselager <philipp.boeselager@web.de>
2022-08-11 13:28:52 +0200

    Add namespace-services (GOKbIT-289)

---
+++ src/shared/services/namespace-services/index.js
@@ -0,0 +1,4 @@
+import baseServices from '@/shared/services/base-services'
+import api from './namespace-services'
+
+export default api(baseServices)
---
+++ src/shared/services/namespace-services/namespace-services.js
@@ -0,0 +1,29 @@
+import namespacesModel from '@/shared/models/namespaces-model'
+
+const PLATFORM_URL = '/rest/identifier-namespaces'
+
+const requestNamespaces = (baseServices, cancelToken) => {
+  return baseServices.request({
+    method: 'GET',
+    url: process.env.VUE_APP_API_BASE_URL + `${PLATFORM_URL}`,
+  }, cancelToken);
+}
+
+const api = (baseServices) => ({
+  fetchNamespacesList (cancelToken) {
+    var allNss = requestNamespaces(baseServices, cancelToken)
+    for (ns of allNss.data) {
+      console.log("Adding namespace: " + JSON.stringify(ns))
+      namespacesModel.addNamespace(ns)
+    }
+  },
+  getBaseurl(nsVal) {
+    var ns = namespacesModel.getNamespace(nsVal)
+    if (ns?.baseUrl){
+      return ns.baseUrl
+    }
+    return null
+  }
+})
+
+export default api
