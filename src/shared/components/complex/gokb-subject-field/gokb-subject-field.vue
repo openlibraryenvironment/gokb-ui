a9dc6efdf6293b6e9c91f5c89f61112ba7ecccd2
Moritz Horn <moritz.horn@gbv.de>
2023-11-15 15:41:01 +0100

    add subjects section

---
+++ src/shared/components/complex/gokb-subject-field/gokb-subject-field.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <v-row>
+      <gokb-state-field
+        v-model="localScheme"
+        width="150px"
+        class="ms-4"
+        message-path="component.subject.scheme"
+        :label="$tc('component.subject.scheme.label')"
+        return-object
+        required
+      />
+    </v-row>
+    <v-row>
+      <v-col>
+        <gokb-text-field
+          v-model="localHeading"
+          :disabled="disabled"
+          :label="$tc('component.subject.heading.label')"
+          required
+          @click:append="$emit('delete', value)"
+        />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+  import GokbTextField from '@/shared/components/base/gokb-text-field'
+  import GokbSelectField from '../../base/gokb-select-field/gokb-select-field.vue'
+
+  export default {
+    name: 'GokbSubjectField',
+    components: { GokbTextField, GokbSelectField },
+    props: {
+      disabled: {
+        type: Boolean,
+        required: false,
+        default: false
+      },
+      deleteable: {
+        type: Boolean,
+        required: false
+      },
+      value: {
+        type: Object,
+        required: true,
+        default: () => ({ scheme: undefined, heading: undefined })
+      },
+    },
+    computed: {
+      deleteIcon () {
+        return this.deleteable ? 'delete' : undefined
+      },
+      localHeading: {
+        get () {
+          return this.value.heading
+        },
+        set (heading) {
+          this.$emit('input', { heading, scheme: this.value.scheme })
+        }
+      },
+      localScheme: {
+        get () {
+          return this.value.scheme
+        },
+        set (scheme) {
+          this.$emit('input', { heading: this.value.heading, scheme })
+        }
+      },
+    }
+  }
+</script>
