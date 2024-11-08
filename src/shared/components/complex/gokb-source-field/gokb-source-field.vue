9c9fea5145d82f965b16538005000909f98d82ab
Moritz Horn <moritz.horn@gbv.de>
2024-11-08 16:02:56 +0100

    fix init

--- src/shared/components/complex/gokb-source-field/gokb-source-field.vue
+++ src/shared/components/complex/gokb-source-field/gokb-source-field.vue
@@ -174,7 +174,7 @@
 
       if (!!this.modelValue?.id) {
         this.fetch(this.modelValue.id)
-      } else if (!!this.modelValue.url) {
+      } else if (!!this.modelValue?.url) {
         this.isExpanded = true
       }
     },
