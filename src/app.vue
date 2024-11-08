33912bc5bc384c68822f77603d2baf59fd51b97a
Moritz Horn <moritz.horn@gbv.de>
2024-11-08 18:55:06 +0100

    adjust package & fix env variable

--- src/app.vue
+++ src/app.vue
@@ -297,7 +297,7 @@
       privacyLink: import.meta.env.VITE_DP_LINK,
       imprintLink: import.meta.env.VITE_IMP_LINK,
       docsLink: import.meta.env.VITE_DOCS_LINK,
-      accessibilityLink: import.meta.VITE_APP_ACC_LINK,
+      accessibilityLink: import.meta.VITE_ACC_LINK,
       appName: import.meta.env.VITE_TITLE || 'GOKb Client',
       appColor: import.meta.env.VITE_COLOR || '#4f4f4f',
       appVersion: pkg.version || import.meta.env.VITE_VERSION,
