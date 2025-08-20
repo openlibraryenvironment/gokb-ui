23e1401ec508cc078a257ab376d3b5d415637551
christophkohl1 <christoph.kohl@hbz-nrw.de>
2025-08-20 18:01:04 +0200

    add search select field for FTP configs

---
+++ src/shared/components/simple/gokb-search-webendpoint-field/gokb-search-webendpoint-field.vue
@@ -0,0 +1,19 @@
+<script>
+import GokbSelectField from '@/shared/components/base/gokb-select-field'
+
+export default {
+  name: 'GokbSearchWebendpointField',
+  extends: GokbSelectField,
+  props: {
+    label: {
+      type: String,
+      required: false,
+      default: 'Endpoint Configs',
+    }
+  },
+  created () {
+    this.entityName = 'webHookEndpoint'
+  }
+
+}
+</script>
